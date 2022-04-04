25728ce8a970b0f545b3610cc85b6701db745b57
Rahul <rahulkotr1@gmail.com>
2022-04-04 21:20:44 +0530

    React portfolio

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import Layout from './components/Layout'
-import './App.scss'
+import Layout from 'D:/28_GitHub/ReactPortfolio/react-portfolio/src/components/Layout'
+import 'D:/28_GitHub/ReactPortfolio/react-portfolio/src/App.scss'
 import { Routes, Route} from 'react-router-dom'
 
 function App() {
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,7 +1,12 @@
 import './index.scss'
+import Sidebar from '../Sidebar'
 
 const Layout = () => {
-    return <> Hellooooo</>
+    return (
+        <> 
+            <Sidebar />
+        </>
+    ) 
 }
 
 export default Layout
